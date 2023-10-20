f0a28f55adb70d35a7d149a3d95fe62e21f6c9e4
Matt Nodurfth <mnodurft@gmail.com>
2023-10-20 10:28:21 -0700

    Move to m.

--- basic.sh
+++ basic.sh
@@ -45,9 +45,9 @@ alias glgp='git log --stat -p'
 alias ggpush='git push origin "$(git_current_branch)"'
 alias ggpull='git pull origin "$(git_current_branch)"'
 
-alias mn-tail-logs="tail $(ls -rt /var/log/ -I 'wtmp*' -I '*.gz' -I 'dpkg*gz' -I 'postgresql') -vf"
+alias m.tail-logs="tail $(ls -rt /var/log/ -I 'wtmp*' -I '*.gz' -I 'dpkg*gz' -I 'postgresql') -vf"
 
-mn-tail-logsf() {
+m.tail-logsf() {
 	tail $(ls -rt /var/log/ -I 'wtmp*' -I '*.gz' -I 'dpkg*gz' -I 'postgresql') -vf
 }
 
@@ -91,56 +91,58 @@ alias v.='nvim .'
 alias sv='sudo nvim'
 
 #Networking
-alias mn-whatsmyip="curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'"
-alias mn-whereami-latlong='curl ipinfo.io/loc'
-alias mn-whereami-country='curl ipinfo.io/country'
-alias mn-whereami='curl ipinfo.io/json'
+alias m.whatsmyip="curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'"
+alias m.whereami-latlong='curl ipinfo.io/loc'
+alias m.whereami-country='curl ipinfo.io/country'
+alias m.whereami='curl ipinfo.io/json'
 
 # Layer 1: Physical - Is our physical interface up?
-alias mn-net1-interfaces='ip -br link show'
-alias mn-net1-interfaces-more='ifconfig'
-mn-net1-interface-up() {
+alias m.net1-interfaces='ip -br link show'
+alias m.net1-interfaces-more='ifconfig'
+m.net1-interface-up() {
 	#if down:
 	ip link set $1 up
 }
-mn-net1-interface-more() {
+m.net1-interface-more() {
 	#show additional stats like dropped packets
 	ip -s link show $1
 }
 
 # Layer 2: Data Link 'local Network'
-alias mn-net2-neighbors='ip neighbor show' # Check ARP Table
-mn-net2-neighbor-recheck() {
+alias m.net2-neighbors='ip neighbor show' # Check ARP Table
+m.net2-neighbor-recheck() {
 	# args: IP dev interface
 	#force new ARP resolution by deleting the record
 	ip neighbor delete $1 $2 $3
 }
 
 # Layer 3: Network/Internet
-alias mn-net3-ip-address='ip -br address show' # 1st check local IP address; rules out DHCP or misconfig issues
-mn-net3-ping() {
+alias m.net3-ip-address='ip -br address show' # 1st check local IP address; rules out DHCP or misconfig issues
+m.net3-ping() {
 	# Start troubleshooting remote host resolution
 	ping $1
 }
-mn-net3-traceroute() {
+m.net3-traceroute() {
 	# Next check the route to the remote host with
 	traceroute $1
 }
 
-alias mn-net3-gateway='ip route show' #check routing table for upstream gateways
-mn-net3-gateway-check() {
+alias m.net3-gateway='ip route show' #check routing table for upstream gateways
+m.net3-gateway-check() {
 	# check the route for a specific prefix:
 	#ip route show 10.0.0.0/8
 	ip route show $1
 }
-mn-net3-dns-check() {
+m.net3-dns-check() {
 	#DNS not L3 protocol
 	##nslookup www.google.com
 	nslookup $1
 }
 
 # Layer 4: Transport - TCP/UDP
-alias mn-net4-ports-local='ss -tunlp4' #check local listening ports
+#
+alias m.net4-httpstat="httpstat"      #check curl connection statistics
+alias m.net4-ports-local='ss -tunlp4' #check local listening ports
 # -t - Show TCP ports.
 # -u - Show UDP ports.
 # -n - Do not try to resolve hostnames.
@@ -148,19 +150,19 @@ alias mn-net4-ports-local='ss -tunlp4' #check local listening ports
 # -p - Show the processes that are using a particular socket.
 # -4 - Show only IPv4 sockets.
 
-alias mn-net4-ports-local-process='sudo netstat -tulpn' #show process
-mn-net4-ports-remote() {
+alias m.net4-ports-local-process='sudo netstat -tulpn' #show process
+m.net4-ports-remote() {
 	#$1 ip/host
 	sudo nmap -sTU -O $1
 }
-mn-net4-port-connection-tcp() {
+m.net4-port-connection-tcp() {
 	# telnet database.example.com 3306
 	# $1 ip/host
 	# $2 port
 	telnet $1 $2
 }
 
-mn-net4-port-connection-udp() {
+m.net4-port-connection-udp() {
 	# Danger, uninstall after using nc
 	# nc 192.168.122.1 -u 80
 	# $1 ip/host
